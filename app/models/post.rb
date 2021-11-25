2e97001d383e11b9b7edb9f68cdd37567c3ae3db
garrettdick <garrettd0511@gmail.com>
2021-11-24 19:13:13 -0600

    commit tree a52f2ff5253d56a103be59ed88acc2c4803fbbf6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :click_bait 
+    def click_bait
+        if title 
+            unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any?{|phrase| title.include? phrase}
+                errors.add(:title, "This is not enough clickbait")
+            end
+        end
+    end
+    # def click_bait
+#     baits = [“Won’t Believe”,“Secret”,“Top [number]“,”Guess”]
+#     if title
+#       if baits.none? {|bait| title.include?(bait)}
+#         errors.add(:title, “not click bait enough”)
+#       end
+#     end
+#   end
 end
